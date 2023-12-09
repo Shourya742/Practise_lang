a693f6b374202373e0909618f4629d7f4001eb46
Shourya742 <sshourya17@gmail.com>
2023-12-09 20:57:57 +0530

    Added basic crud

--- rust_wd/target/debug/deps/rust_wd-7da046bbe6c32c0c.d
+++ rust_wd/target/debug/deps/rust_wd-7da046bbe6c32c0c.d
@@ -1,5 +1,6 @@
-/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-7da046bbe6c32c0c.rmeta: src/main.rs
+/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-7da046bbe6c32c0c.rmeta: src/main.rs src/./question.json
 
-/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-7da046bbe6c32c0c.d: src/main.rs
+/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-7da046bbe6c32c0c.d: src/main.rs src/./question.json
 
 src/main.rs:
+src/./question.json:
--- rust_wd/target/debug/deps/rust_wd-f235f2dca2ff872f.d
+++ rust_wd/target/debug/deps/rust_wd-f235f2dca2ff872f.d
@@ -1,5 +1,6 @@
-/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-f235f2dca2ff872f.rmeta: src/main.rs
+/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-f235f2dca2ff872f.rmeta: src/main.rs src/./question.json
 
-/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-f235f2dca2ff872f.d: src/main.rs
+/Users/shoushar/practise/rust_wd/target/debug/deps/rust_wd-f235f2dca2ff872f.d: src/main.rs src/./question.json
 
 src/main.rs:
+src/./question.json:
