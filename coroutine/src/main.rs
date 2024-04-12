988e2961550cc5cadc53772cb41ca495a334374e
Shourya Sharma <sshourya17@gmail.com>
2024-04-12 04:43:09 -0400

    Added symmetric coroutine

--- coroutine/src/main.rs
+++ coroutine/src/main.rs
@@ -92,6 +92,30 @@ impl CoroutineManager {
     }
 }
 
+trait SymmetricCoroutine {
+    type Input;
+    type Output;
+    fn resume_with_input(self: Pin<&mut Self>, input: Self::Input) -> Self::Output;
+}
+
+impl SymmetricCoroutine for ReadCoroutine {
+    type Input = ();
+    type Output = Option<i32>;
+
+    fn resume_with_input(mut self: Pin<&mut Self>, input: Self::Input) -> Self::Output {
+        if let Some(Ok(line)) = self.lines.next() { line.parse::<i32>().ok() } else { None }
+    }
+}
+
+impl SymmetricCoroutine for WriteCoroutine {
+    type Input = i32;
+    type Output = ();
+
+    fn resume_with_input(mut self: Pin<&mut Self>, input: Self::Input) -> Self::Output {
+        writeln!(self.file_handle, "{}", input).unwrap()
+    }
+}
+
 fn append_number_to_file(n: i32) -> io::Result<()> {
     let mut file = OpenOptions::new().create(true).append(true).open("numbers.txt")?;
     writeln!(file, "{}", n)?;
@@ -118,8 +142,19 @@ fn main() -> io::Result<()> {
     //     }
     // }
 
-    let mut manager = CoroutineManager::new("numbers.txt", "output.txt").unwrap();
-    manager.run();
+    // let mut manager = CoroutineManager::new("numbers.txt", "output.txt").unwrap();
+    // manager.run();
+
+    let mut reader = ReadCoroutine::new("numbers.txt")?;
+    let mut writer = WriteCoroutine::new("output.txt");
+    loop {
+        let number = Pin::new(&mut reader).resume_with_input(());
+        if let Some(num) = number {
+            Pin::new(&mut writer).resume_with_input(num);
+        } else {
+            break;
+        }
+    }
 
     Ok(())
 }
