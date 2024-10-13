619c9e1237441759a2221902ddac1027d3fce84f
bit-aloo <sshourya17@gmail.com>
2024-10-13 09:13:58 +0530

    adding weak pointers

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -1,14 +1,90 @@
+use std::cell::UnsafeCell;
 use std::ops::Deref;
 use std::ptr::NonNull;
 use std::sync::atomic::{fence, AtomicUsize};
 
-pub struct Arc<T> {
+pub struct Weak<T> {
     ptr: NonNull<ArcData<T>>,
 }
 
+unsafe impl<T: Sync + Send> Send for Weak<T> {}
+unsafe impl<T: Sync + Send> Sync for Weak<T> {}
+
+impl<T> Weak<T> {
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self
+            .data()
+            .data_ref_count
+            .load(std::sync::atomic::Ordering::Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX);
+            if let Err(e) = self.data().data_ref_count.compare_exchange_weak(
+                n,
+                n + 1,
+                std::sync::atomic::Ordering::Relaxed,
+                std::sync::atomic::Ordering::Relaxed,
+            ) {
+                n = e;
+                continue;
+            }
+            return Some(Arc { weak: self.clone() });
+        }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self
+            .data()
+            .alloc_ref_count
+            .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
+            > usize::MAX / 2
+        {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        let ptr = self.weak.data().data.get();
+
+        unsafe { (*ptr).as_ref().unwrap() }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self
+            .data()
+            .alloc_ref_count
+            .fetch_sub(1, std::sync::atomic::Ordering::Release)
+            == 1
+        {
+            fence(std::sync::atomic::Ordering::Acquire);
+            unsafe { drop(Box::from_raw(self.ptr.as_ptr())) }
+        }
+    }
+}
+
+pub struct Arc<T> {
+    weak: Weak<T>,
+}
+
 struct ArcData<T> {
-    ref_count: AtomicUsize,
-    data: T,
+    data_ref_count: AtomicUsize,
+    alloc_ref_count: AtomicUsize,
+    data: UnsafeCell<Option<T>>,
 }
 
 unsafe impl<T: Send + Sync> Send for Arc<T> {}
@@ -17,64 +93,66 @@ unsafe impl<T: Send + Sync> Sync for Arc<T> {}
 impl<T> Arc<T> {
     pub fn new(data: T) -> Arc<T> {
         Arc {
-            ptr: NonNull::from(Box::leak(Box::new(ArcData {
-                ref_count: AtomicUsize::new(1),
-                data,
-            }))),
+            weak: Weak {
+                ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                    alloc_ref_count: AtomicUsize::new(1),
+                    data_ref_count: AtomicUsize::new(1),
+                    data: UnsafeCell::new(Some(data)),
+                }))),
+            },
         }
-    }
-
-    fn data(&self) -> &ArcData<T> {
-        unsafe { self.ptr.as_ref() }
     }
 
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
         if arc
+            .weak
             .data()
-            .ref_count
+            .alloc_ref_count
             .load(std::sync::atomic::Ordering::Relaxed)
             == 1
         {
             fence(std::sync::atomic::Ordering::Acquire);
-            unsafe { Some(&mut arc.ptr.as_mut().data) }
+            let arcdata = unsafe { arc.weak.ptr.as_mut() };
+            let option = arcdata.data.get_mut();
+            let data = option.as_mut().unwrap();
+            Some(data)
         } else {
             None
         }
     }
-}
 
-impl<T> Deref for Arc<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.data().data
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        arc.weak.clone()
     }
 }
 
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
-        if self
+        let weak = self.weak.clone();
+        if weak
             .data()
-            .ref_count
+            .data_ref_count
             .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
             > usize::MAX / 2
         {
             std::process::abort();
         }
-        Arc { ptr: self.ptr }
+        Arc { weak }
     }
 }
 
 impl<T> Drop for Arc<T> {
     fn drop(&mut self) {
         if self
+            .weak
             .data()
-            .ref_count
+            .data_ref_count
             .fetch_sub(1, std::sync::atomic::Ordering::Release)
             == 1
         {
             fence(std::sync::atomic::Ordering::Acquire);
-            unsafe { drop(Box::from_raw(self.ptr.as_ptr())) }
+            let ptr = self.weak.data().data.get();
+            unsafe { (*ptr) = None }
         }
     }
 }
