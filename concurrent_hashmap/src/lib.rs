ac658e401385ffbf8e55ffeadeb04d85d78effc2
bit-aloo <sshourya17@gmail.com>
2024-10-29 11:58:42 +0530

    add put and insert

--- concurrent_hashmap/src/lib.rs
+++ concurrent_hashmap/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{hash::{BuildHasher, DefaultHasher, Hash, Hasher, RandomState}, sync::atomic::Ordering};
 
-use crossbeam::epoch::{Atomic, Guard, Shared};
+use crossbeam::epoch::{Atomic, Guard, Owned, Shared};
+use node::{BinEntry, Node};
 
 /// The largest possible table capacity. This value must be
 /// exactly 1<<30 to stay within Java array allocation and indexing
@@ -46,10 +47,14 @@ pub struct SHashMap<K,V, S = RandomState> {
 }
 
 impl<K,V,S> SHashMap<K, V, S> where S: BuildHasher, K: Hash {
-    pub fn get<'g>(&'g self, key: &K, guard: &'g Guard) -> Option<Shared<'g, V>> {
+
+    fn hash(&self, key: &K) -> u64 {
         let mut hasher = self.build_hasher.build_hasher();
         key.hash(&mut hasher);
-        let hasher = hasher.finish();
+        hasher.finish()
+    }
+    pub fn get<'g>(&'g self, key: &K, guard: &'g Guard) -> Option<Shared<'g, V>> {
+        let hasher = self.hash(key);
         let table = self.table.load(std::sync::atomic::Ordering::SeqCst, guard);
         if table.is_null() {
             return None;
@@ -58,9 +63,8 @@ impl<K,V,S> SHashMap<K, V, S> where S: BuildHasher, K: Hash {
             return None;
         }
 
-        let mask = table.bins.len() as u64 - 1;
-        let bini = (hasher & mask) as u128;
-        let bin = table.at(bini, guard);
+        let bini = table.bini(hasher);
+        let bin = table.bin(bini, guard);
         if bin.is_null() {
             return None;
         }
@@ -72,6 +76,105 @@ impl<K,V,S> SHashMap<K, V, S> where S: BuildHasher, K: Hash {
         assert!(!v.is_null());
         Some(v)
     }
+
+
+    pub fn insert(&self, key: K, value: V) -> Option<()> {
+        todo!()
+    }
+
+    pub fn put(&self, key: K, value: V, no_replacement: bool) -> Option<()> {
+        let h = self.hash(&key);
+        let mut bin_count = 0;
+        let guard = crossbeam::epoch::pin();
+        let mut table = self.table.load(Ordering::SeqCst, _&guard);
+        let mut node = Owned::new(BinEntry::Node(Node {
+            key,
+            value: Owned::new(value),
+            hash:h,
+            next: Atomic::null()
+        }));
+        let old_value = loop {   
+            if table.is_null() || table.bins.len() == 0 {
+                let table = self.init_table(guard);
+                continue;
+            }
+            let bini = table.bini(h);
+            let mut bin = table.bin(bini, guard);
+            while bin.is_null() {
+                // fast path -> bin is empty so stock us at the front
+                match table.cas_bin(bini, bin, node, guard) {
+                    Ok(_old_null_ptr) => {
+                        self.add_count(1,0);
+                        return None;}
+                    Err(changed) => {
+                        assert!(!changed.current.is_null());
+                        node = changed.new;
+                        bin = changed.current;
+                    }
+                }
+            }
+
+            match *bin {
+                BinEntry::Moved(next_table) => {
+                    table = table.help_transfer(next_table);
+                    unimplemented!()
+                }
+                BinEntry::Node(ref head) if no_replacement && head.hash == h && &head.key == &node.key => {
+                    // fast path if replacement is disallowed and first bin matches
+                    return Some(())
+                }
+                BinEntry::Node(ref head) => {
+                    // bin is non-empty need to link into it, so we must take the lock
+                    let _guard = head.lock.lock();
+                    // need to check taht this is _still_ the head
+                    let current_head = table.bin(bini,guard);
+                    if current_head.as_raw() != bin.as_raw() {
+                        /// nope --> try again from the start
+                        continue;
+                    }
+
+                    // yes, it is still the head, we can now "own" the bin
+                    // note that there can still be readers in the bin
+                    // no other writters.
+
+                    // TODO: TreeBin && ReservationNode
+                    let mut bin_count = 1;
+                    let mut n = head;
+                    let old_val = loop {
+                        if n.hash == node.hash && n.key == node.key {
+                            // The key already exist in the map!
+                            if no_replacement {
+                                // the key is not absent, so dont update
+                            } else {
+                                let now_garbage = n.value.swap(node.value, Ordering::SeqCst, &guard);
+                                unimplemented!("need to dispose of garbage");
+
+                            }
+                            break Some(())
+                        }
+                        // TODO: This Ordering can probably be relaxed due to the mutex
+                        let next = n.next.load(Ordering::SeqCst, &guard);
+
+                        if next.is_null() {
+                            // we're at the end of the bin -- stick the node here!!
+                            n.next.store(node, Ordering::SeqCst);
+                            break None;
+                        }
+                        n = next;
+                        bin_count+=1;
+                    };
+
+                    // TODO: TREEIFY_THRESHILD
+                    if old_val.is_none() {
+                        // Increment counter
+                        self.add_count(1, bin_count);
+                    }
+                    break old_val;
+                }
+            }
+        }
+        todo!()
+    }
 }
 
 struct Table<K,V,S> {
@@ -79,7 +182,20 @@ struct Table<K,V,S> {
 }
 
 impl<K,V,S> Table<K,V,S> {
-    fn at<'g>(&self, i:usize, guard: &'g Guard) -> Shared<'g,node::BinEntry<K,V>> {
+
+    #[inline]
+    fn bini(&self, hash: u64) -> usize {
+        let mask = self.bins.len() as u64 - 1;
+        (hash & mask) as usize
+    }
+
+    #[inline]
+    fn bin<'g>(&self, i:usize, guard: &'g Guard) -> Shared<'g,node::BinEntry<K,V>> {
         self.bins[i].load(std::sync::atomic::Ordering::Acquire, guard)
+    }
+
+    #[inline]
+    fn cas_bin<'g>(&self, i: usize, current: Shared<node::BinEntry<K,V>>, new: Owned<node::BinEntry<K,V>>, guard: &'g Guard) -> Result<Shared<'g, node::BinEntry<K,V>>, crossbeam::epoch::CompareAndSetError<'g, node::BinEntry<K,V>, Owned<node::BinEntry<K,V>>>> {
+        self.bins[i].compare_and_set(current, new, Ordering::AcqRel, guard)
     }
 }
--- concurrent_hashmap/src/node.rs
+++ concurrent_hashmap/src/node.rs
@@ -1,4 +1,5 @@
 use crossbeam::epoch::{Atomic, Guard, Shared};
+use parking_lot::{Mutex, RawMutex};
 use std::cell::UnsafeCell;
 
 
@@ -7,6 +8,7 @@ use std::cell::UnsafeCell;
 /// will _generally_ be `Node`. Any entry that is not first in the bin, will be a `Node`
 pub(crate) enum BinEntry<K,V> {
     Node(Node<K,V>),
+    Moved(*const super::Table<K,V>)
 }
 
 
@@ -16,15 +18,9 @@ where K:Eq
     pub(crate) fn find<'g>(&'g self, hash: u64, key: &K, guard: &'g Guard)-> Shared<'g, Node<K,V>> {
         match *self {
             BinEntry::Node(ref n) => {
-                if n.hash == hash && &n.key == key {
-                    return Shared::from(n as *const _);
-                }
-                let next = n.next.load(std::sync::atomic::Ordering::SeqCst, guard);
-                if next.is_null() {
-                    return Shared::null();
-                }
-                return next;
+                return n.find(hash,key,guard);
             }
+            BinEntry::Moved(_) => todo!()
         }
     }
 }
@@ -34,5 +30,19 @@ pub(crate) struct Node<K,V> {
     pub(crate) hash: u64,
     pub(crate) key: K,
     pub(crate) value: Atomic<V>,
-    pub(crate) next: Atomic<Node<K,V>>
+    pub(crate) next: Atomic<Node<K,V>>,
+    pub(crate) lock: Mutex<()>
+}
+
+impl<K,V> Node<K,V> where K: Eq{
+    pub(crate) fn find<'g>(&'g self, hash: u64, key: &K, guard: &'g Guard) -> Shared<'g, Node<K,V>> {
+        if self.hash == hash && &self.key == key {
+            return Shared::from(self as *const _);
+        }
+        let next = self.next.load(std::sync::atomic::Ordering::SeqCst, guard);
+        if next.is_null() {
+            return Shared::null();
+        }
+        return next.find(hash,key,guard);
+    }
 }
