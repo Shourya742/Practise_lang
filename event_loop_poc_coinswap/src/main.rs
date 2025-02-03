711dcc4790f5fc49d2520b67dd395057cd7b3c1f
bit-aloo <sshourya17@gmail.com>
2025-02-03 18:16:45 +0530

    sleeping for less CPU utilization but still bad

--- event_loop_poc_coinswap/src/main.rs
+++ event_loop_poc_coinswap/src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unreachable_code)]
 #![allow(dead_code)]
 #![allow(unused_variables)]
-use std::{io::ErrorKind, net::TcpListener};
+use std::{io::ErrorKind, net::TcpListener, time::Duration};
 
 fn main() -> anyhow::Result<()> {
     let stream = TcpListener::bind(("127.0.0.1", 5555))?;
@@ -11,6 +11,7 @@ fn main() -> anyhow::Result<()> {
         let (client, address) = match stream.accept() {
             Ok(ret) =>  ret,
             Err(error) if error.kind() == ErrorKind::WouldBlock => {
+                std::thread::sleep(Duration::from_millis(100));
                 continue;
             }
             Err(error) => return Err(error.into())
