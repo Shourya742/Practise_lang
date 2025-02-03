d712d00ae4f3048d71b048c3dde9b120e15f6fea
bit-aloo <sshourya17@gmail.com>
2025-02-03 18:15:41 +0530

    blocking code with non_blocking stream

--- event_loop_poc_coinswap/src/main.rs
+++ event_loop_poc_coinswap/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unreachable_code)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use std::{io::ErrorKind, net::TcpListener};
+
+fn main() -> anyhow::Result<()> {
+    let stream = TcpListener::bind(("127.0.0.1", 5555))?;
+    stream.set_nonblocking(true)?;
+
+    loop {
+        let (client, address) = match stream.accept() {
+            Ok(ret) =>  ret,
+            Err(error) if error.kind() == ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(error) => return Err(error.into())
+        };
+
+        println!("Connected from {address}");
+    }
+    Ok(())
 }
