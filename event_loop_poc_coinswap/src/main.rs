ee1d816c5e5413e613f2249b9fbccd55fcd98fbb
bit-aloo <sshourya17@gmail.com>
2025-02-03 19:54:34 +0530

    add timeout to connections, and let epoll sleep based on those..

--- event_loop_poc_coinswap/src/main.rs
+++ event_loop_poc_coinswap/src/main.rs
@@ -8,7 +8,7 @@ use std::{
     net::{SocketAddr, TcpListener},
     os::fd::{AsRawFd, RawFd},
     sync::Arc,
-    time::Duration,
+    time::{Duration, Instant},
 };
 
 use epoll::{Event, Events};
@@ -18,6 +18,16 @@ struct Client {
     reader: MessageReader<ClientToServerMsg>,
     address: SocketAddr,
     connected: bool,
+    last_activity: Instant
+}
+
+const TIMEOUT_DURATION: Duration =  Duration::from_secs(4);
+
+impl Client {
+    fn until_timeout(&self) -> Duration {
+        let since_last_activity = self.last_activity.elapsed();
+        TIMEOUT_DURATION.saturating_sub(since_last_activity)
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -43,8 +53,18 @@ fn main() -> anyhow::Result<()> {
 
         let mut events = [Event::new(Events::empty(), 0); 1024];
 
+        // Max time till we make the epoll blocking based on disconnection from the socket.
+        // -1 for infinite, like till any of the fd gets ready
+        // t minimum disconnection time.
+        let time_until_next_timeout = clients.iter().map(|c| c.until_timeout()).map(|d| d.as_millis() as i32).min().unwrap_or(-1);
+
+        eprintln!("Time until timeout: {time_until_next_timeout}");
+
         // This will wait until any of the filedescriptors reader socket fulfills.
-        let event_count = epoll::wait(epoll, -1, &mut events)?;
+        // For coinswap we have a shutdown schemeing, so makes sense to have a timeout to epoll
+        // rather then being blocking.
+        // default epoll is level triggered, should we do edge trigger.. not sure..
+        let event_count = epoll::wait(epoll, time_until_next_timeout, &mut events)?;
 
         for event in &events[..event_count] {
             let fd = event.data as RawFd;
@@ -76,41 +96,33 @@ fn main() -> anyhow::Result<()> {
                     reader: MessageReader::<ClientToServerMsg>::new(client.clone()),
                     address,
                     connected: true,
+                    last_activity: Instant::now()
                 });
                 continue;
             }
 
             for client in &mut clients {
                 if fd == client.reader.stream.as_raw_fd() {
-                    let msg = match client.reader.recv() {
-                        Some(Ok(msg)) => msg,
-                        Some(Err(error)) if error.kind() == ErrorKind::WouldBlock => {
-                            continue;
-                        }
-                        Some(Err(error)) => {
-                            eprintln!("Client {} ended with error: {error:?}", client.address);
-                            client.connected = false;
-                            continue;
-                        }
-                        None => {
-                            client.connected = false;
-                            continue;
-                        }
-                    };
-        
-                    eprintln!("Received msg: {msg:?}");
+                    handle_client(client).unwrap();   
                 }
             }
+        }
 
-
+        for client in &mut clients {
+            if client.until_timeout() == Duration::ZERO {
+                client.connected = false;
+            }
         }
 
         clients.retain(|c|{
             if !c.connected {
                 epoll::ctl(epoll, epoll::ControlOptions::EPOLL_CTL_DEL, c.reader.stream.as_raw_fd(), Event::new(Events::EPOLLIN, c.reader.stream.as_raw_fd() as u64)).unwrap();
                 c.reader.stream.shutdown(std::net::Shutdown::Both).unwrap();
+                eprintln!("Client disconnected: {}", c.address);
             }
-             c.connected});
+            c.connected
+
+            });
     }
     Ok(())
 }
@@ -123,4 +135,29 @@ enum ClientToServerMsg {
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 enum ServerToClientMsg {
     Pong,
+}
+
+
+fn handle_client(client: &mut Client) -> anyhow::Result<()> {
+    let msg = match client.reader.recv() {
+        Some(Ok(msg)) => msg,
+        Some(Err(error)) if error.kind() == ErrorKind::WouldBlock => {
+            return Ok(());
+        }
+        Some(Err(error)) => {
+            eprintln!("Client {} ended with error: {error:?}", client.address);
+            client.connected = false;
+            return Ok(());
+        }
+        None => {
+            client.connected = false;
+            return Ok(());
+        }
+    };
+
+    client.last_activity = Instant::now();
+
+    eprintln!("Received msg: {msg:?}");   
+
+    Ok(())
 }
