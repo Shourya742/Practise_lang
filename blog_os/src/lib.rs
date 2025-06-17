5d6069c38fe60a7c192e83ad910e54d80d425c39
bit-aloo <sshourya17@gmail.com>
2025-06-17 09:38:02 +0530

    hardware interrupts

--- blog_os/src/lib.rs
+++ blog_os/src/lib.rs
@@ -10,6 +10,8 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 
+pub mod gdt;
+
 use core::panic::PanicInfo;
 
 pub trait Testable {
@@ -39,7 +41,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 /// Entry point for `cargo test`
@@ -48,7 +50,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     init();  
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -75,5 +77,15 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
+    unsafe {interrupts::PICS.lock().initialize()};
+    x86_64::instructions::interrupts::enable();
+}
+
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
