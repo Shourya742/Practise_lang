2704ab4c71baa8516ab2cc2a834716faf16323c0
bit-aloo <sshourya17@gmail.com>
2025-09-21 17:11:40 +0530

    add parser logic

--- sql_parser/src/expr.rs
+++ sql_parser/src/expr.rs
@@ -1,4 +1,7 @@
-use crate::token::TokenKind;
+use crate::{
+    parser::{ParseResult, Parser},
+    token::TokenKind,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Literal<'a> {
@@ -21,6 +24,26 @@ pub enum BinaryOp {
     Minus,
     Multiply,
     Divide,
+}
+
+impl BinaryOp {
+    pub fn from_token(token: TokenKind) -> Option<Self> {
+        match token {
+            TokenKind::And => Some(BinaryOp::And),
+            TokenKind::Or => Some(BinaryOp::Or),
+            TokenKind::Equal => Some(BinaryOp::Equal),
+            TokenKind::NotEqual => Some(BinaryOp::NotEqual),
+            TokenKind::Less => Some(BinaryOp::Less),
+            TokenKind::Greater => Some(BinaryOp::Greater),
+            TokenKind::LessEqual => Some(BinaryOp::LessEqual),
+            TokenKind::GreaterEqual => Some(BinaryOp::GreaterEqual),
+            TokenKind::Plus => Some(BinaryOp::Plus),
+            TokenKind::Minus => Some(BinaryOp::Minus),
+            TokenKind::Star => Some(BinaryOp::Multiply),
+            TokenKind::Slash => Some(BinaryOp::Divide),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -91,5 +114,91 @@ impl std::fmt::Display for BinaryOp {
             BinaryOp::Divide => "/",
         };
         write!(f, "{}", s)
+    }
+}
+
+impl<'a> Parser<'a> {
+    /// Parse an expression using precedence climbing
+    pub fn parse_expr(&mut self) -> ParseResult<Expr<'a>> {
+        self.parse_expr_with_precedence(0)
+    }
+
+    /// Parse expression with minimum precedence
+    fn parse_expr_with_precedence(&mut self, min_prec: u8) -> ParseResult<Expr<'a>> {
+        let mut left = self.parse_primary()?;
+
+        while let Some(token) = self.current() {
+            if let Some((prec, is_left)) = get_precedence(token.kind) {
+                if prec < min_prec {
+                    break;
+                }
+
+                let op_kind = token.kind;
+                self.advance();
+                let next_min_prec = if is_left { prec + 1 } else { prec };
+                let right = self.parse_expr_with_precedence(next_min_prec)?;
+
+                if let Some(op) = BinaryOp::from_token(op_kind) {
+                    left = Expr::Binary {
+                        left: Box::new(left),
+                        op,
+                        right: Box::new(right),
+                    };
+                }
+            } else {
+                break;
+            }
+        }
+
+        Ok(left)
+    }
+
+    fn parse_primary(&mut self) -> ParseResult<Expr<'a>> {
+        match self.current() {
+            Some(token) => {
+                match token.kind {
+                    TokenKind::Number => {
+                        let text = token.text;
+                        self.advance();
+                        let n = text
+                            .parse::<i64>()
+                            .map_err(|_| self.error_at_current("Invalid number"))?;
+                        Ok(Expr::Literal(Literal::Number(n)))
+                    }
+                    TokenKind::Float => {
+                        let text = token.text;
+                        self.advance();
+                        let f = text
+                            .parse::<f64>()
+                            .map_err(|_| self.error_at_current("Invalid float"))?;
+                        Ok(Expr::Literal(Literal::Float(f)))
+                    }
+                    TokenKind::String => {
+                        let text = token.text;
+                        self.advance();
+                        // Remove quotes
+                        let s = &text[1..text.len() - 1];
+                        Ok(Expr::Literal(Literal::String(s)))
+                    }
+                    TokenKind::Identifier => {
+                        let text = token.text;
+                        self.advance();
+                        Ok(Expr::Column(text))
+                    }
+                    TokenKind::Star => {
+                        self.advance();
+                        Ok(Expr::Star)
+                    }
+                    TokenKind::LeftParen => {
+                        self.advance();
+                        let expr = self.parse_expr()?;
+                        self.expect(TokenKind::RightParen)?;
+                        Ok(Expr::Paren(Box::new(expr)))
+                    }
+                    _ => Err(self.error_at_current("Expected expression")),
+                }
+            }
+            None => Err(self.error_at_current("Unexpected end of input")),
+        }
     }
 }
--- sql_parser/src/parser.rs
+++ sql_parser/src/parser.rs
@@ -1,5 +1,7 @@
 use crate::{
+    ast::{CTE, Query, SelectStmt, Statement, TableRef, With},
     error::{Backtrace, ParseError},
+    expr::Expr,
     token::{Token, TokenKind, tokenize},
 };
 
@@ -165,11 +167,239 @@ impl<'a> Parser<'a> {
         }
         false
     }
+
+    pub fn parse_query(&mut self) -> ParseResult<Query<'a>> {
+        if self.current().map(|t| t.kind) == Some(TokenKind::With) {
+            let with = self.parse_with()?;
+            let query = Box::new(self.parse_query()?);
+            Ok(Query::With { with, query })
+        } else {
+            let select = self.parse_select()?;
+
+            if self.try_consume(TokenKind::Union) {
+                let all = self.try_consume(TokenKind::All);
+                let right = Box::new(self.parse_query()?);
+                Ok(Query::Union {
+                    left: Box::new(Query::Select(Box::new(select))),
+                    all,
+                    right,
+                })
+            } else {
+                Ok(Query::Select(Box::new(select)))
+            }
+        }
+    }
+
+    fn parse_identifier_lis(&mut self) -> ParseResult<Vec<&'a str>> {
+        let mut idents = vec![self.parse_identifier()?];
+
+        while self.try_consume(TokenKind::Comma) {
+            idents.push(self.parse_identifier()?);
+        }
+        Ok(idents)
+    }
+
+    fn parse_cte(&mut self) -> ParseResult<CTE<'a>> {
+        let name = self.parse_identifier()?;
+        let columns = if self.current().map(|t| t.kind) == Some(TokenKind::LeftParen) {
+            self.advance();
+            let cols = self.parse_identifier_lis()?;
+            self.expect(TokenKind::RightParen)?;
+            Some(cols)
+        } else {
+            None
+        };
+
+        self.expect(TokenKind::As)?;
+        self.expect(TokenKind::LeftParen)?;
+        let query = Box::new(self.parse_query()?);
+        self.expect(TokenKind::RightParen)?;
+        Ok(CTE {
+            name,
+            columns,
+            query,
+        })
+    }
+
+    pub fn parse_with(&mut self) -> ParseResult<With<'a>> {
+        self.expect(TokenKind::With)?;
+
+        let recursive = self.try_consume(TokenKind::Recursive);
+
+        let mut ctes = vec![self.parse_cte()?];
+
+        while self.try_consume(TokenKind::Comma) {
+            ctes.push(self.parse_cte()?);
+        }
+        Ok(With { recursive, ctes })
+    }
+
+    fn parse_expr_list(&mut self) -> ParseResult<Vec<Expr<'a>>> {
+        let mut exprs = vec![self.parse_expr()?];
+
+        while self.try_consume(TokenKind::Comma) {
+            exprs.push(self.parse_expr()?);
+        }
+        Ok(exprs)
+    }
+
+    fn parse_table_ref(&mut self) -> ParseResult<TableRef<'a>> {
+        let name = self.parse_identifier()?;
+
+        let alias = if self.try_consume(TokenKind::As) {
+            Some(self.parse_identifier()?)
+        } else if let Some(token) = self.current() {
+            if token.kind == TokenKind::Identifier {
+                if self.check_for_specific_where_typos() {
+                    return Err(self.backtrace.get_error(self.input));
+                }
+                Some(self.parse_identifier()?)
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        Ok(TableRef { name, alias })
+    }
+
+    pub fn parse_select(&mut self) -> ParseResult<SelectStmt<'a>> {
+        let mut had_errors = false;
+
+        match self.current() {
+            Some(token) if token.kind == TokenKind::Select => {
+                self.advance();
+            }
+            Some(token) if token.kind == TokenKind::Identifier => {
+                self.backtrace.track_error(
+                    token.span.start,
+                    "SELECT",
+                    Some(token.text),
+                    self.input,
+                );
+                had_errors = true;
+
+                let text = token.text.to_uppercase();
+                if text.starts_with("SEL") && text.len() >= 4 {
+                    self.advance();
+                } else {
+                    return Err(self.backtrace.get_error(self.input));
+                }
+            }
+            Some(token) => {
+                self.backtrace.track_error(
+                    token.span.start,
+                    "SELECT",
+                    Some(token.text),
+                    self.input,
+                );
+                return Err(self.backtrace.get_error(self.input));
+            }
+            None => {
+                let pos = if self.pos > 0 && !self.tokens.is_empty() {
+                    self.tokens[self.pos - 1].span.end
+                } else {
+                    0
+                };
+                self.backtrace.track_error(pos, "SELECT", None, self.input);
+                return Err(self.backtrace.get_error(self.input));
+            }
+        }
+
+        let projection = if self.try_consume(TokenKind::Star) {
+            vec![Expr::Star]
+        } else {
+            self.parse_expr_list()?
+        };
+
+        let from = if self.try_consume(TokenKind::From) {
+            Some(self.parse_table_ref()?)
+        } else {
+            if self.check_for_keyword_typo("FROM", &['F']) {
+                return Err(self.backtrace.get_error(self.input));
+            }
+            None
+        };
+
+        let where_clause = if self.try_consume(TokenKind::Where) {
+            Some(self.parse_expr()?)
+        } else {
+            if self.check_for_where_typo() {
+                return Err(self.backtrace.get_error(self.input));
+            }
+            None
+        };
+
+        if had_errors {
+            return Err(self.backtrace.get_error(self.input));
+        }
+
+        Ok(SelectStmt {
+            projection,
+            from,
+            where_clause,
+        })
+    }
+
+    pub fn parse_statement(&mut self) -> ParseResult<Statement<'a>> {
+        let start_pos = self.pos;
+
+        if self.current().map(|t| t.kind) == Some(TokenKind::With) {
+            match self.parse_with() {
+                Ok(with) => match self.parse_query() {
+                    Ok(query) => {
+                        return Ok(Statement::Query(Query::With {
+                            with,
+                            query: Box::new(query),
+                        }));
+                    }
+                    Err(_) => {
+                        self.pos = start_pos;
+                    }
+                },
+                Err(_) => {
+                    self.pos = start_pos;
+                }
+            }
+        }
+
+        self.pos = start_pos;
+        match self.parse_select() {
+            Ok(stmt) => return Ok(Statement::Query(Query::Select(Box::new(stmt)))),
+            Err(_) => {}
+        }
+        self.pos = start_pos;
+        if let Some(token) = self.current() {
+            self.backtrace
+                .track_error(token.span.start, "INSERT", Some(token.text), self.input);
+            self.backtrace
+                .track_error(token.span.start, "UPDATE", Some(token.text), self.input);
+            self.backtrace
+                .track_error(token.span.start, "DELETE", Some(token.text), self.input);
+            self.backtrace
+                .track_error(token.span.start, "WITH", Some(token.text), self.input);
+        }
+
+        Err(self.backtrace.get_error(self.input))
+    }
 }
 
 pub fn parse_sql(sql: &str) -> Result<(), ParseError> {
     let tokens = tokenize(sql);
     let backtrace = Backtrace::new();
     let mut parser = Parser::new(&tokens, &backtrace, sql);
+    let _stmt = parser.parse_statement()?;
     Ok(())
+}
+
+pub fn parse_sql_to_string(sql: &str) -> Result<String, ParseError> {
+    use crate::token::tokenize;
+
+    let tokens = tokenize(sql);
+    let backtrace = Backtrace::new();
+    let mut parser = Parser::new(&tokens, &backtrace, sql);
+
+    let stmt = parser.parse_statement()?;
+    Ok(format!("{:?}", stmt))
 }
