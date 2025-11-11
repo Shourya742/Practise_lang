31c9d010894f5f28d6d057a7962f0d95e8d3ef56
bit-aloo <sshourya17@gmail.com>
2025-11-11 21:50:12 +0530

    add builder

---
+++ meta/src/queue/builder.rs
@@ -0,0 +1,77 @@
+use std::marker::PhantomData;
+
+use crate::queue::{single::QueueSingle, st_queue::StQueue};
+
+pub struct QueueBuilder<Target>
+where
+    Target: StQueue,
+{
+    target: PhantomData<Target>,
+}
+
+impl<Target> Default for QueueBuilder<Target>
+where
+    Target: StQueue,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<Target> QueueBuilder<Target>
+where
+    Target: StQueue,
+{
+    pub fn new() -> Self {
+        Self {
+            target: Default::default(),
+        }
+    }
+
+    pub fn push(
+        self,
+        element: Target::Front,
+    ) -> QueueBuilding<Target, Target::Back, QueueSingle<Target::Front>> {
+        QueueBuilding::new(QueueSingle::new(element))
+    }
+}
+
+pub struct QueueBuilding<Target, Remaining, Current>
+where
+    Target: StQueue,
+    Remaining: StQueue,
+    Current: StQueue,
+{
+    target: PhantomData<Target>,
+    remaining: PhantomData<Remaining>,
+    current: Current,
+}
+
+impl<Target, Remaining, Current> QueueBuilding<Target, Remaining, Current>
+where
+    Target: StQueue,
+    Remaining: StQueue,
+    Current: StQueue,
+{
+    fn new(current: Current) -> Self {
+        Self {
+            target: Default::default(),
+            remaining: Default::default(),
+            current,
+        }
+    }
+
+    pub fn push(
+        self,
+        element: Remaining::Front,
+    ) -> QueueBuilding<Target, Remaining::Back, Current::PushBack<Remaining::Front>> {
+        QueueBuilding::new(self.current.push(element))
+    }
+
+    pub fn finish(self) -> Current
+    where
+        Target: StQueue<Front = Current::Front, Back = Current::Back>,
+    {
+        self.current
+    }
+}
--- meta/src/queue/mod.rs
+++ meta/src/queue/mod.rs
@@ -1,3 +1,4 @@
+pub mod builder;
 pub mod multi;
 pub mod single;
 pub mod st_queue;
